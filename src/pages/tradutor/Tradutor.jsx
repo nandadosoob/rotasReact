b49d54675e5e0fd4730731bb4d6997efca0e4c6d
nandaBe <130059223+nandadosoob@users.noreply.github.com>
2024-10-16 11:08:06 -0300

    finalizeii

--- src/pages/tradutor/Tradutor.jsx
+++ src/pages/tradutor/Tradutor.jsx
@@ -5,10 +5,6 @@ import { Pagina } from "../../components/Pagina"
 export function Tradutor() {
 
     const [inputValue, setInputValue] = useState("")
-    // function contaCaracteres(e){
-    // setInputValue(e.target.value.length)
-    // }
-
     const [valueTraduz, setValueTraduz] = useState("")
     function traduz() {
         fetch(
@@ -17,7 +13,7 @@ export function Tradutor() {
             .then((resposta) => resposta.json())
             .then((dados) => setValueTraduz(dados.responseData.translatedText))
             .catch((error) => {
-                alert("Erro ao traduzir dados:", error);
+                alert("Erro:", error);
             });
         console.log(valueTraduz);
 
@@ -30,7 +26,9 @@ export function Tradutor() {
                 <div>
                     <h3>Português</h3>
                     <div className="w-60 h-60 border-2 border-red-800 rounded-md bg-red-100 flex flex-col">
-                        <input type="text" className="bg-red-100 h-52 shadow-none outline-none p-2" maxLength={250} onChange={(evento) => setInputValue(evento.target.value)} value={inputValue} />
+                        <input type="text" className="bg-red-100 h-52 shadow-none outline-none p-2" maxLength={250} onChange={(evento) => setInputValue(evento.target.value)} value={inputValue} 
+                        
+                        />
                         <div className="flex flex-row justify-between p-2">
                             <span className="text-gray-400">{inputValue.length}/250</span>
                             <button className="w-28 h-10 border-2 rounded-md border-red-900 bg-red-400" onClick={traduz}>Traduzir</button>
@@ -40,8 +38,8 @@ export function Tradutor() {
 
                 <div>
                     <h3>Inglês</h3>
-                    <div className="w-60 h-60 border-2 border-red-800 rounded-md bg-red-100 flex flex-col justify-center">
-                        <span>{valueTraduz}</span>
+                    <div className="w-60 h-60 border-2 border-red-800 rounded-md bg-red-100 ">
+                        <span className="p-2 h-48 flex flex-col justify-center">{valueTraduz}</span>
                     </div>
 
                 </div>
